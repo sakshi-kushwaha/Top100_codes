796e474b1ffcc52e2eedaac24464589abc67477b
sakshi-kushwaha <sakshikushwaha21@gmail.com>
2023-11-03 23:52:50 +0530

    Even-odd and sum of n numbers

---
+++ Numbers/even-odd.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int num;
+    cout<<"Enter the number:";
+    cin>>num;
+    
+    if(num%2==0){
+        cout<<"even";
+    }
+    else{
+        cout<<"odd";
+    }
+
+    return 0;
+}
---
+++ Numbers/sum-n-numbers.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int num, ans=0;
+    cout<<"Enter the number:";
+    cin>>num;
+    
+    for (int i=1;i<=num;i++){
+        ans=ans+i;
+    }
+    cout<<ans;
+    
+    return 0;
+}
